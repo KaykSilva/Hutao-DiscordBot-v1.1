672be836ef9888ea860e17486b555af2505f93c3
Kayk <kayksilva489@gmail.com>
2024-03-11 19:26:31 -0300

    Custom welcome command

---
+++ src/commands/admin/welcomeConfiguration.js
@@ -0,0 +1,163 @@
+const { ActionRowBuilder, StringSelectMenuBuilder, StringSelectMenuOptionBuilder, SlashCommandBuilder } = require('discord.js');
+const fs = require('fs');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName("welcome")
+        .setDescription('Customização para mensagem de boas-vindas'),
+    async execute(interaction) {
+        const guildId = interaction.guild.id; // Obtenha a ID da guilda
+
+        const select = new StringSelectMenuBuilder()
+            .setCustomId('starter')
+            .setPlaceholder('Selecione o campo para customizar.')
+            .addOptions(
+                new StringSelectMenuOptionBuilder()
+                    .setLabel('Chat')
+                    .setDescription('Selecione o chat para exibir a mensagem.')
+                    .setValue('id_channel'),
+
+                new StringSelectMenuOptionBuilder()
+                    .setLabel('Descrição')
+                    .setDescription('Mude a frase de recepção')
+                    .setValue('description'),
+
+                new StringSelectMenuOptionBuilder()
+                    .setLabel('Banner')
+                    .setDescription('Mude o banner de exibição')
+                    .setValue('banner'),
+
+                    new StringSelectMenuOptionBuilder()
+                    .setLabel('Titulo 1')
+                    .setDescription('Destaque um chat na mensagem')
+                    .setValue('title1'),
+
+                new StringSelectMenuOptionBuilder()
+                    .setLabel('Titulo 2')
+                    .setDescription('Destaque um chat na mensage')
+                    .setValue('title2'),
+                    
+                new StringSelectMenuOptionBuilder()
+                    .setLabel('Titulo 3')
+                    .setDescription('Destaque um chat na mensage')
+                    .setValue('title3'),
+
+                new StringSelectMenuOptionBuilder()
+                    .setLabel('Canal 1')
+                    .setDescription('Destaque um chat na mensagem')
+                    .setValue('channel1'),
+
+                new StringSelectMenuOptionBuilder()
+                    .setLabel('Canal 2')
+                    .setDescription('Selecione o canal para o título 2')
+                    .setValue('channel2'),
+
+                new StringSelectMenuOptionBuilder()
+                    .setLabel('Canal 3')
+                    .setDescription('Selecione o canal para o título 3')
+                    .setValue('channel3'),
+
+            );
+
+        const row = new ActionRowBuilder()
+            .addComponents(select);
+
+        await interaction.reply({
+            content: 'Configuração do comando welcome',
+            components: [row],
+        });
+
+        const filter = (i) => i.customId === 'starter' && i.user.id === interaction.user.id;
+        const collector = interaction.channel.createMessageComponentCollector({
+            filter,
+            time: 15000, // Tempo em milissegundos para aguardar interações
+        });
+
+        const settings = {}; // Objeto para armazenar as configurações temporariamente
+
+        collector.on('collect', async (i) => {
+            const selectedValue = i.values[0];
+
+
+            if (selectedValue === 'id_channel') {
+                await interaction.followUp(`Por favor, mencione o canal que deseja exibir as mensagens`);
+
+            } else if (selectedValue === 'description') {
+                await interaction.followUp(`Envie sua recepção calorosa!`);
+
+            } else if (selectedValue === 'title1') {
+                await interaction.followUp(`De um nome ao chat ex: "Regras"`);
+
+            } else if (selectedValue === 'title2') {
+                await interaction.followUp(`De um nome ao chat ex: "Regras"`);
+
+            } else if (selectedValue === 'title3') {
+                await interaction.followUp(`De um nome ao chat ex: "Regras"`);
+                
+            } else if (selectedValue === 'channel1') {
+                await interaction.followUp(`Mencione o canal correspondente ao chat1`);
+                
+            } else if (selectedValue === 'channel2') {
+                await interaction.followUp(`Mencione o canal correspondente ao chat2`);
+                
+            } else if (selectedValue === 'channel3') {
+                await interaction.followUp(`Mencione o canal correspondente ao chat3`);
+                
+            } else if (selectedValue === 'banner') {
+                await interaction.followUp(`Envie o link da imagem ou gif`);
+                
+            }
+    
+            // Aguarde a resposta do usuário
+            const responseCollectorFilter = (msg) => msg.author.id === interaction.user.id;
+
+            const responseCollector = interaction.channel.createMessageCollector({
+                filter: responseCollectorFilter,
+                time: 15000,
+            });
+
+
+
+            responseCollector.on('collect', (msg) => {
+                const response = msg.content.trim();
+
+                // Salve as configurações temporariamente
+                settings[selectedValue] = response;
+
+                // Finalize o coletor de resposta
+                responseCollector.stop();
+            });
+
+            responseCollector.on('end', () => {
+                // Finalize o coletor principal
+                collector.stop();
+            });
+        });
+
+        collector.on('end', (collected, reason) => {
+            if (reason === 'time') {
+                // Usuário não respondeu a tempo
+                interaction.followUp('Tempo expirado. A configuração não foi concluída.');
+            } else {
+                
+                // Atualize o arquivo JSON com as configurações coletadas
+                const filePath = `./src/db/welcome/welcome_${guildId}.json`; // Use a ID da guilda no nome do arquivo
+                let existingSettings = {};
+
+                try {
+                    const data = fs.readFileSync(filePath, 'utf-8');
+                    existingSettings = JSON.parse(data);
+                    interaction.followUp('Configurações salvas com sucesso!');
+                } catch (error) {
+                    console.error('Erro ao ler o arquivo JSON:', error);
+                }
+
+                const updatedSettings = { ...existingSettings, ...settings };
+
+                fs.writeFileSync(filePath, JSON.stringify(updatedSettings, null, 2), 'utf-8');
+
+
+            }
+        });
+    },
+};
--- src/events/guildMemberAdd.js
+++ src/events/guildMemberAdd.js
@@ -1,23 +1,38 @@
 const { Events, AttachmentBuilder, EmbedBuilder } = require('discord.js');
+const fs = require('fs');
 
 module.exports = {
     name: Events.GuildMemberAdd,
     async execute(member) {
-        const channel_id = '1216572060344647755';
-        const channel = member.guild.channels.cache.get(channel_id);
-        
+
+        const filePath = `./src/db/welcome/welcome_${member.guild.id}.json`;
+        let config;
+        try {
+            const data = fs.readFileSync(filePath, 'utf-8');
+            config = JSON.parse(data);
+            
+        } catch (error) {
+            console.error('Erro ao ler o arquivo JSON:', error);
+            return;
+        }
+
+        try {
+            config.id_channel = config.id_channel.replace(/[^\w\s]/gi, '');
+        } catch (error) {
+            console.error('Erro ao manipular o ID do canal:', error);
+            return;
+        }
+
         //channels config
-        const title1 = 'Registro'
-        const title2 = 'a'
-        const title3 = ''
-
-        const channel1 = '<#1216824085237989546>'
-        const channel2 = '<#1216824085237989546>'
-        const channel3 = ''
-
-
-
-        if (!channel) return;
+        const description = config['description'] || '';
+        const id_channel = config['id_channel'] || '';
+        const banner = config['banner'] || 'https://i.pinimg.com/originals/f1/ac/04/f1ac043d7cabf6224a3cde8b34734d81.gif';
+        const title1 = config['title1'] || '';
+        const title2 = config['title2'] || '';
+        const title3 = config['title3'] || '';
+        const channel1 = `${config['channel1'] || ''}`;
+        const channel2 = `${config['channel2'] || ''}`;
+        const channel3 = `${config['channel3'] || ''}`;
 
         // Obtenha a URL do avatar do usuário
         const userAvatarUrl = member.user.displayAvatarURL({ format: 'png', size: 512 });
@@ -27,14 +42,21 @@ module.exports = {
             .setTitle(`Bem-vindo a ${member.guild.name} `)
             .setURL('https://discord.gg/dAXKWdPnnh')
             .setAuthor({ name: member.user.username, iconURL: userAvatarUrl, url: 'https://discord.js.org' })
-            .setDescription(`Hey ${member.user.tag}, se perdeu por aqui \n\n ${title1} ${channel1} \n\n  ${title2} ${channel2} \n\n  ${title3} ${channel3} \n\n   ` )
+
+            .setDescription(`Hey ${member.user.tag}, ${description} \n\n ${title1} ${channel1} \n\n  ${title2} ${channel2} \n\n  ${title3} ${channel3} \n\n   `)
             .setThumbnail(userAvatarUrl)
-            
-        
-            
-            .setImage('https://i.pinimg.com/originals/f1/ac/04/f1ac043d7cabf6224a3cde8b34734d81.gif') // Define a imagem do embed como o avatar do usuário
+            .setImage(banner) // Define a imagem do embed como o avatar do usuário
             .setTimestamp()
             .setFooter({ text: 'Entrou:', iconURL: userAvatarUrl });
+            
+            const channel_id = id_channel;
+            const channel = member.guild.channels.cache.get(channel_id);
+
+            if (!channel) {
+                console.error('Canal não encontrado. Verifique se o ID do canal está correto.');
+                member.followUp
+                return;
+            }
 
         channel.send({ embeds: [exampleEmbed] });
     }
